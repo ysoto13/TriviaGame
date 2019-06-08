4144fe152227ff3fe8e384c56b47af75ccbd66e2
Yadira Soto <lunar13heart@hotmail.com>
2019-06-07 21:35:51 -0400

    edited a bit more

--- assets/javscript/app.js
+++ assets/javscript/app.js
@@ -18,193 +18,169 @@
 
 
 // A $( document ).ready() block.
-$(document).ready(function(){
+$(document).ready(function () {
     console.log("ready!");
-};
-
-
-// WORK AREA
-var card = $("quiz");
-
-var countStartNumber = 90;
-
-var question = [{
-
-    question1: "What are the names of the 7 dwarfs in the movie Snow White & the Seven Dwarfs?",
-    answers: ["Doc, Snotty, Grumpy, Dopey, Sneezy, Sadness, & Shy", "Doc, Sleepy, Grandpappy, Dopey, Allergy, Happy, & Tacky", "Doc, Sleepy, Grumpy, Dopey, Flowey, Picky, & Frightful", "Doc, Sleepy, Grumpy, Dopey, Sneezy, Happy, & Bashful"],
-    // correctAnswer: "Doc, Sleepy, Grumpy, Dopey, Sneezy, Happy, & Bashful"
-},
-
-
-{
-    question2: "What color is Rapunzel's dress?",
-    answers: ["pink", "yellow", "purple", "ice blue"],
-    // correctAnswer: "purple"
-},
-
-
-{
-    question3: "Which princess knows how to cook?",
-    answers: ["Tiana", "Ariel", "Cinderella", "Elsa"],
-    // correctAnswer: "Tiana"
-},
-
-
-{
-    question4: "Rafiki, is from which Disney movie?",
-    answers: ["The Little Mermaid", "The Lion King", "Frozen", "The Jungle Book"],
-    // correctAnswer: "The Lion King"
-},
-
-
-{
-    question5: "Where did the Beauty and the Beast movie take place?",
-    answers: ["France", "Germany", "Norway", "Sweden"],
-    // correctAnswer: "France"
-},
-
-
-{
-    question6: "Which movie has the song I'll Make a Man Out of You?",
-    answers: ["Aladdin", "Lilo & Stitch", "Lady And the Tramp", "Mulan"],
-    // correctAnswer: "Mulan"
-},
-
-
-{
-    question7: "What is the name of Mufasa's brother?",
-    answers: ["T'Challa", "Sin", "Scar", "Al"],
-    // correctAnswer: "Scar"
-},
-
-
-{
-    question8: "What is the name of Moana's pet rooster?",
-    answers: ["Chicken", "Hei Hei", "Maui", "Pua"],
-    // correctAnswer: "Hei Hei"
-
-},
-
-
-{
-    question9: "What is the name of Lilo's older sister?",
-    answers: ["Nana", "Lola", "Nani", "Nala"],
-    // correctAnswer: "Nani"
-},
-
-
-{
-    question10: "In the movie, brave what is MErida's horse called?",
-    answers: ["Hubert", "Harris", "Hamish", "Angus"],
-    // correctAnswer: "Angus"
-}];
+    $("#start-button").click(function (){
 
 
 
+    // WORK AREA
+    var countStartNumber = 90;
 
-//button
-$(document).on("click", ".btn btn-light", function () {
-    question.clicked();
-    console.log;
-});
+    var question = [{
+
+        question1: "What are the names of the 7 dwarfs in the movie Snow White & the Seven Dwarfs?",
+        answers: ["Doc, Snotty, Grumpy, Dopey, Sneezy, Sadness, & Shy", "Doc, Sleepy, Grandpappy, Dopey, Allergy, Happy, & Tacky", "Doc, Sleepy, Grumpy, Dopey, Flowey, Picky, & Frightful", "Doc, Sleepy, Grumpy, Dopey, Sneezy, Happy, & Bashful"],
+        // correctAnswer: "Doc, Sleepy, Grumpy, Dopey, Sneezy, Happy, & Bashful"
+    },
+
+
+    {
+        question2: "What color is Rapunzel's dress?",
+        answers: ["pink", "yellow", "purple", "ice blue"],
+        // correctAnswer: "purple"
+    },
+
+
+    {
+        question3: "Which princess knows how to cook?",
+        answers: ["Tiana", "Ariel", "Cinderella", "Elsa"],
+        // correctAnswer: "Tiana"
+    },
+
+
+    {
+        question4: "Rafiki, is from which Disney movie?",
+        answers: ["The Little Mermaid", "The Lion King", "Frozen", "The Jungle Book"],
+        // correctAnswer: "The Lion King"
+    },
+
+
+    {
+        question5: "Where did the Beauty and the Beast movie take place?",
+        answers: ["France", "Germany", "Norway", "Sweden"],
+        // correctAnswer: "France"
+    },
+
+
+    {
+        question6: "Which movie has the song I'll Make a Man Out of You?",
+        answers: ["Aladdin", "Lilo & Stitch", "Lady And the Tramp", "Mulan"],
+        // correctAnswer: "Mulan"
+    },
+
+
+    {
+        question7: "What is the name of Mufasa's brother?",
+        answers: ["T'Challa", "Sin", "Scar", "Al"],
+        // correctAnswer: "Scar"
+    },
+
+
+    {
+        question8: "What is the name of Moana's pet rooster?",
+        answers: ["Chicken", "Hei Hei", "Maui", "Pua"],
+        // correctAnswer: "Hei Hei"
+
+    },
+
+
+    {
+        question9: "What is the name of Lilo's older sister?",
+        answers: ["Nana", "Lola", "Nani", "Nala"],
+        // correctAnswer: "Nani"
+    },
+
+
+    {
+        question10: "In the movie, brave what is Merida's horse called?",
+        answers: ["Hubert", "Harris", "Hamish", "Angus"],
+        // correctAnswer: "Angus"
+    }];
 
 
 
 
 
-// A variable to hold the set interval
-var timer;
+    // A variable to hold the set interval
+    function start();
+    counter = setInterval(timer, 1000 * 9);
 
-var game = {
-    questions: questions,
-    currentQuestions: 0,
-    counter: CounterStartNumber,
-    correct: 0,
-    incorrect: 0,
 
-    countdown: function(){
-        // decrement counter
-        game.counter --;
+    var game = {
+        questions: questions,
+        currentQuestions: 0,
+        counter: CounterStartNumber,
+        correct: 0,
+        incorrect: 0,
+
+        countdown: function () {
+            // decrement counter
+            game.counter--;
             // use jquery to put dynamically put logic onto the page
-                // if time is up, run time up function
-    },
+            // if time is up, run time up function
+            $("show").html("<h1>" + number + "</h1>");
+            if (number === 0) {
+                stop();
+            }
+        },
 
-    timeUp: function(){
-        // clearInterval(timer)
-        // use jquery to change the text of the game counter
-        // dynamically add out of time to the card
-        // append the Correct anaswer to the card
-        // append image/gif to the card
-    },
+        timeUp: function () {
+            // clearInterval(timer)
+            clearInterval(countdown);
+        },
 
-    results: function(){
-        // clearInterval
-        // dynamically add htmml to let them know of there results
-        // use jquery to add htmtl of game.counter to the id of counter-number
-        // add how many correct answers they got
-        // add how many incorrect answer they got
-        // add a start over button
-    },
+        results: function () {
+            // clearInterval
 
-    clicked: function(){
-        // clearInterval(timer)
-        // if/else statement for when an answer is clicked
+            // dynamically add htmml to let them know of there results
+            // use jquery to add htmtl of game.counter to the id of counter-number
+            // add how many correct answers they got
+            // add how many incorrect answer they got
+            // add a start over button
+        },
+
+        clicked: function () {
+            // clearInterval(timer)
+            clearInterval(countdown);
+            // if/else statement for when an answer is clicked
+            
             // if correct run answeredCorrectly() function
             // else run answeredIncorrectly() function
-    },
+        },
 
-    answeredIncorrectly: function(){
-        // insert text here so look at notes
-        // if else statement is to freeze the image & timer before moving on
-        // add a point to the incorrect column
-        // clearInterval(timer)
-        // dynamically add html to let them know they are wrong
-        // add the right answer
-        // add the image 
-        // if / else statment
-            // if no more question wait three seconds then show result
-            // else wait three seconds and show next question
-    },
+        answeredIncorrectly: function () {
+        
+            // clearInterval(timer)
+            // if / else statment
+        },
 
-    answeredCorrectly: function(){
-        // insert text here so look at notes
-        // if else statement is to freeze the image & timer before moving on
-        // add a point to correct column
-        // clearInterval(timer);
-        // dynamically add html to let them know they are correct
-        // add image
-        // if / else statment
-            // if no more question wait three seconds then show result
-            // else wait three seconds and show next question
+        answeredCorrectly: function () {
+            // clearInterval(timer);
+            // if / else statment
+        },
 
-    },
-
-    reset: function(){}
-};
+        reset: function () { }
+    };
 
 
 
 
-// // Create Click Events
-//START BUTTON
-// on click even here jq
-$(document).on("click", "#start", function() {
-    $("#start").prepend("<h2>Time Remaining: <span id='counter-number'>90</span> Seconds</h2>");
-    game.loadQuestion();
-    console.log;
+    // // Create Click Events
+    //START BUTTON
+    // on click even here jq
+    $("#start").on("click", start);
+
+
+    //SUBMIT BUTTON
+    // on click even here jq
+    $("#submit-button").on("click", submit-button);
+
+    //START OVER BUTTON
+    // on click even here jq
+    $("#start-over").on("click", start-over);
+
+    start();
+
 });
-
-
-//SUBMIT BUTTON
-// on click even here jq
-$(document).on("click", ".submit-button", function (e) {
-    game.clicked(e);
-    console.log;
-});
-
-//START OVER BUTTON
-// on click even here jq
-$(document).on("click", "#start-over", function () {
-    game.reset();
-    console.log;
 });
