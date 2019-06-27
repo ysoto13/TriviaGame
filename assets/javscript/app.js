ef79f4b6207fa54d66688e75be26f7f593ad0829
Yadira Soto <lunar13heart@hotmail.com>
2019-06-26 22:55:30 -0400

    edits

--- assets/javscript/app.js
+++ assets/javscript/app.js
@@ -20,185 +20,162 @@
 // A $( document ).ready() block.
 $(document).ready(function () {
     console.log("ready!");
-    $("#start-button").click(function () {
-
-
-
-        // WORK AREA
-        var card = $("#quiz");
-
-        var countStartNumber = 90;
-
-        var correctAnswers = ["Doc, Sleepy, Grumpy, Dopey, Sneezy, Happy, & Bashful", "purple", "Tiana", "The Lion King", "France", "Mulan", "Scar", "Hei Hei", "Nani", "Angus"];
-
-        var question = [{
-
-            question1: "What are the names of the 7 dwarfs in the movie Snow White & the Seven Dwarfs?",
-            answers: ["Doc, Snotty, Grumpy, Dopey, Sneezy, Sadness, & Shy", "Doc, Sleepy, Grandpappy, Dopey, Allergy, Happy, & Tacky", "Doc, Sleepy, Grumpy, Dopey, Flowey, Picky, & Frightful", "Doc, Sleepy, Grumpy, Dopey, Sneezy, Happy, & Bashful"],
-            // correctAnswer: "Doc, Sleepy, Grumpy, Dopey, Sneezy, Happy, & Bashful"
-        },
-
-
-        {
-            question2: "What color is Rapunzel's dress?",
-            answers: ["pink", "yellow", "purple", "ice blue"],
-            // correctAnswer: "purple"
-        },
-
-
-        {
-            question3: "Which princess knows how to cook?",
-            answers: ["Tiana", "Ariel", "Cinderella", "Elsa"],
-            // correctAnswer: "Tiana"
-        },
-
-
-        {
-            question4: "Rafiki, is from which Disney movie?",
-            answers: ["The Little Mermaid", "The Lion King", "Frozen", "The Jungle Book"],
-            // correctAnswer: "The Lion King"
-        },
-
-
-        {
-            question5: "Where did the Beauty and the Beast movie take place?",
-            answers: ["France", "Germany", "Norway", "Sweden"],
-            // correctAnswer: "France"
-        },
-
-
-        {
-            question6: "Which movie has the song I'll Make a Man Out of You?",
-            answers: ["Aladdin", "Lilo & Stitch", "Lady And the Tramp", "Mulan"],
-            // correctAnswer: "Mulan"
-        },
-
-
-        {
-            question7: "What is the name of Mufasa's brother?",
-            answers: ["T'Challa", "Sin", "Scar", "Al"],
-            // correctAnswer: "Scar"
-        },
-
-
-        {
-            question8: "What is the name of Moana's pet rooster?",
-            answers: ["Chicken", "Hei Hei", "Maui", "Pua"],
-            // correctAnswer: "Hei Hei"
-
-        },
-
-
-        {
-            question9: "What is the name of Lilo's older sister?",
-            answers: ["Nana", "Lola", "Nani", "Nala"],
-            // correctAnswer: "Nani"
-        },
-
-
-        {
-            question10: "In the movie, brave what is Merida's horse called?",
-            answers: ["Hubert", "Harris", "Hamish", "Angus"],
-            // correctAnswer: "Angus"
-        }];
+});
 
 
 
 
+// WORK AREA
+var card = $("#quiz");
 
 
-        // A variable to hold the set interval
-        function start() {
-            clearInterval(intervalId);
-            intervalId = setInterval(decrement, 1000 * 9);
-        }
-
-
-        var game = {
-            questions: questions,
-            currentQuestions: 0,
-            counter: CounterStartNumber,
-            correct: 0,
-            incorrect: 0,
-
-            countdown: function () {
-                // decrement counter
-                function decrement() {
-                    game.counter--;
-                    $("show").html("<h1>" + number + "</h1>");
-                    if (!clockRunning) {
-                        intervalId = setInterval(count, 1000 * 10);
-                        countdown = true;
-                    }
-                }
-                if (number === 0) {
-                    timeUp();
-                    alert("Time is Up!!");
-                }
-        },
-
-
-            timeUp: function () {
-                clearInterval(intervalId);
+var quizQuestions = [
+    {
+        question: "What are the names of the 7 dwarfs in the movie Snow White & the Seven Dwarfs?",
+        answers: ["Doc, Snotty, Grumpy, Dopey, Sneezy, Sadness, & Shy", "Doc, Sleepy, Grandpappy, Dopey, Allergy, Happy, & Tacky", "Doc, Sleepy, Grumpy, Dopey, Flowey, Picky, & Frightful", "Doc, Sleepy, Grumpy, Dopey, Sneezy, Happy, & Bashful"],
+        correctAnswer: "Doc, Sleepy, Grumpy, Dopey, Sneezy, Happy, & Bashful"
     },
 
-        results: function () {
-            // clearInterval
-            clearInterval(intervalId);
-            $("#results").append("<h2>Results!!</h2>")
-            // dynamically add htmml to let them know of there results
-            // use jquery to add htmtl of game.counter to the id of counter-number
-            $(".counter-number").html(game.counter)
-            // add how many correct answers they got
 
-            // add how many incorrect answer they got
-            // add a start over button
-        },
+    {
+        question: "What color is Rapunzel's dress?",
+        answers: ["pink", "yellow", "purple", "ice blue"],
+        correctAnswer: "purple"
+    },
 
-        clicked: function () {
-            // clearInterval(timer)
-            clearInterval(countdown);
-            // if/else statement for when an answer is clicked
 
-            // if correct run answeredCorrectly() function
-            // else run answeredIncorrectly() function
-        },
+    {
+        question: "Which princess knows how to cook?",
+        answers: ["Tiana", "Ariel", "Cinderella", "Elsa"],
+        correctAnswer: "Tiana"
+    },
 
-        answeredIncorrectly: function () {
 
-            // clearInterval(timer)
-            clearInterval(countdown);
-            // if / else statment
-        },
+    {
+        question: "Rafiki, is from which Disney movie?",
+        answers: ["The Little Mermaid", "The Lion King", "Frozen", "The Jungle Book"],
+        correctAnswer: "The Lion King"
+    },
 
-        answeredCorrectly: function () {
-            // clearInterval(timer);
-            clearInterval(countdown);
-            // if / else statment
-        },
 
-        run();
+    {
+        question: "Where did the Beauty and the Beast movie take place?",
+        answers: ["France", "Germany", "Norway", "Sweden"],
+        correctAnswer: "France"
+    },
+
+
+    {
+        question: "Which movie has the song I'll Make a Man Out of You?",
+        answers: ["Aladdin", "Lilo & Stitch", "Lady And the Tramp", "Mulan"],
+        correctAnswer: "Mulan"
+    },
+
+
+    {
+        question: "What is the name of Mufasa's brother?",
+        answers: ["T'Challa", "Sin", "Scar", "Al"],
+        correctAnswer: "Scar"
+    },
+
+
+    {
+        question: "What is the name of Moana's pet rooster?",
+        answers: ["Chicken", "Hei Hei", "Maui", "Pua"],
+        correctAnswer: "Hei Hei"
+
+    },
+
+
+    {
+        question: "What is the name of Lilo's older sister?",
+        answers: ["Nana", "Lola", "Nani", "Nala"],
+        correctAnswer: "Nani"
+    },
+
+
+    {
+        question: "In the movie, brave what is Merida's horse called?",
+        answers: ["Hubert", "Harris", "Hamish", "Angus"],
+        correctAnswer: "Angus"
+    }
+];
+
+
+
+var timer;
+var game = {
+    correct: 0,
+    incorrect: 0,
+    counter: 120,
+
+    countdown: function () {
+        game.counter--;
+        $("#counter-number").html(game.counter);
+        if (game.counter === 0) {
+            console.log("Times Up!");
+            game.done();
+        }
+    },
+
+
+
+    start: function () {
+        timer = setInterval(game.countdown, 1000);
+
+
+
+
+        $("#sub-wrapper").prepend(
+            "<h2>Time Remaining: <span id='counter-number'>120</span> Seconds</h2>"
+        );
+
+        $("#start").remove();
+
+        for (var i = 0; i < quizQuestions.length; i++) {
+            card.append("<h2>" + quizQuestions[i].question + "</h2>");
+            for (var j = 0; j < quizQuestions[i].answers.length; j++) {
+                card.append("<input type='radio' name='question-" + i + "' value='" + quizQuestions[i].answers[j] + "''>" + quizQuestions[i].answers[j]);
+            }
+        }
+
+        card.append("<button id='done'>Done</button>");
+    },
+
+    done: function () {
+        var inputs = card.children("input:checked");
+        for (var i = 0; i < inputs.length; i++) {
+            if ($(inputs[i]).val() === quizQuestions[i].correctAnswer) {
+                game.correct++;
+            } else {
+                game.incorrect++;
+            }
+        }
+        this.result();
+    },
+
+
+    result: function () {
+        clearInterval(timer);
+
+        $("#sub-wrapper h2").remove();
+
+        card.html("<h2>Complete!</h2>");
+        card.append("<h3>Correct Answers: " + this.correct + "</h3>");
+        card.append("<h3>Incorrect Answers: " + this.incorrect + "</h3>");
+    }
+
+
 
 };
 
+// CLICK EVENTS
 
-
-
-// // Create Click Events
-//START BUTTON
-// on click even here jq
-$("#start").on("click", start);
-
-
-//SUBMIT BUTTON
-// on click even here jq
-$("#submit-button").on("click", submit - button);
-
-//START OVER BUTTON
-// on click even here jq
-$("#start-over").on("click", start - over);
-
-start();
-
-    });
+$(document).on("click", "#start", function () {
+    game.start();
 });
+
+$(document).on("click", "#done", function () {
+    game.done();
+});
+        
